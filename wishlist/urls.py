262fe9f61a04c14d00bf0ada82f083ff44c63bcb
trisnobp <trisnotyo6@gmail.com>
2022-09-15 12:57:48 +0700

    commit lab 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,17 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import xml_show_wishlist
+from wishlist.views import json_show_wishlist
+from wishlist.views import choose_show_wishlist
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', xml_show_wishlist, name='xml_show_wishlist'),
+    path('json/', json_show_wishlist, name='json_show_wishlist'),
+    path('json/<int:id>', choose_show_wishlist, name='choose_show_wishlist')
+
 ]
+
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
@@ -9,3 +11,20 @@ def show_wishlist(request):
     'nama': 'Trisno Bayu Pamungkas'
     }
     return render(request, "wishlist.html", context)
+
+def xml_show_wishlist(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def json_show_wishlist(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def choose_show_wishlist(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    
+    if id == 1:
+        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+    elif id == 2:
+        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
