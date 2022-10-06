4a090211a9f6483af0a3feef3a7eb072d69d9912
trisnobp <trisnotyo6@gmail.com>
2022-10-06 14:35:05 +0700

    lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,14 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, tambah_wishlist
 from wishlist.views import xml_show_wishlist
 from wishlist.views import json_show_wishlist
 from wishlist.views import choose_show_wishlist
 from wishlist.views import register
 from wishlist.views import login_user
 from wishlist.views import logout_user
+from wishlist.views import show_wishlist_ajax
+from wishlist.views import tambah_wishlist
 
 app_name = 'wishlist'
 
@@ -18,6 +20,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('ajax/', show_wishlist_ajax, name = 'show_wishlist_ajax'),
+    path('ajax/submit/', tambah_wishlist, name = 'tambah_wishlist'),
 
 ]
 
